3236e805d40575cebecabcbc93166c238cf844d2
lim62 <lim62hi@gmail.com>
2023-06-13 14:36:25 +0300

    new_kalkulator

--- kalkulator/script.js
+++ kalkulator/script.js
@@ -1,34 +1,50 @@
-var numbers = [10, 2, 5];
-
-function kalkul(massive) {
+function kalkul(el) {
+    var massive = [count1, count2]
     var res = massive[0];
     var znach = prompt('+, -, *, /');
     if (znach == '+') {
-        for (var i = 0; i < massive.length; i++) {
+        for (var i = 1; i < massive.length; i++) {
             res += massive[i];
         }
-        console.log('Результат вычисления: ' + res);
+        console.log('Результат: ' + res);
+        el.innerHTML = res;
     }
     else if (znach == '-') {
         for (var i = 1; i < massive.length; i++) {
             res -= massive[i];
         }
-        console.log('Результат вычисления: ' + res);
+        console.log('Результат: ' + res);
+        el.innerHTML = res;
     }
     else if (znach == '*') {
         for (var i = 1; i < massive.length; i++) {
             res *= massive[i];
         }
-        console.log('Результат вычисления: ' + res);
+        console.log('Результат: ' + res);
+        el.innerHTML = res;
     }
     else if (znach == '/') {
         for (var i = 1; i < massive.length; i++) {
             res /= massive[i];
         }
-        console.log('Результат вычисления: ' + res);
+        console.log('Результат: ' + res);
+        el.innerHTML = res;
     }
     else {
         res = 'пользователь ничего не хочет делать!';
         console.warn('Результат вычисления: ' + res);
     }
+}
+
+var count1 = 0;
+var count2 = 0;
+
+function counter1(el) {
+    count1++;
+    el.innerHTML = count1;
+}
+
+function counter2(el) {
+    count2++;
+    el.innerHTML = count2;
 }
